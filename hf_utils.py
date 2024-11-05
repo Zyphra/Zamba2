564dfa0baac104c591826d930eec257f0ec04daa
Stijn <167638923+s-smits@users.noreply.github.com>
2024-11-05 23:13:46 +0100

    Update hf_utils.py

--- hf_utils.py
+++ hf_utils.py
@@ -12,6 +12,6 @@ def load_config_hf(model_name):
 
 def load_state_dict_hf(model_name, device=None, dtype=None):
     mapped_device = "cpu" if dtype not in [torch.float32, None] else device
-    WEIGHTS_NAME = "Zamba2_2p7b_direct_from_pytorch.pt"
+    WEIGHTS_NAME = "/workspace/Zamba2-1.2B/pytorch_model.bin"
     resolved_archive_file = cached_file(model_name, WEIGHTS_NAME, _raise_exceptions_for_missing_entries=True, force_download=False)
     return torch.load(resolved_archive_file, map_location=mapped_device)
