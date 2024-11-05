614deed5861944a9a0010db696f0f7ebc89a2a1e
Stijn <167638923+s-smits@users.noreply.github.com>
2024-11-05 23:33:05 +0100

    Update hf_utils.py

--- hf_utils.py
+++ hf_utils.py
@@ -1,15 +1,17 @@
 import json
-import os
 
 import torch
 
+from transformers.utils import CONFIG_NAME
+from transformers.utils.hub import cached_file
+
+
 def load_config_hf(model_name):
-    base_path = "/workspace/Zamba2-1.2B"
-    config_path = os.path.join(base_path, "config.json")
-    return json.load(open(config_path))
+    resolved_archive_file = cached_file(model_name, CONFIG_NAME, _raise_exceptions_for_missing_entries=True, force_download=False)
+    return json.load(open(resolved_archive_file))
 
 def load_state_dict_hf(model_name, device=None, dtype=None):
     mapped_device = "cpu" if dtype not in [torch.float32, None] else device
-    base_path = "/workspace/Zamba2-1.2B"
-    weights_path = os.path.join(base_path, "pytorch_model.bin")
-    return torch.load(weights_path, map_location=mapped_device)
+    WEIGHTS_NAME = "pytorch_model.bin"
+    resolved_archive_file = cached_file(model_name, WEIGHTS_NAME, _raise_exceptions_for_missing_entries=True, force_download=False)
+    return torch.load(resolved_archive_file, map_location=mapped_device)
